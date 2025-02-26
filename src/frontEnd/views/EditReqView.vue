185bcf38cf79ad75e61fb9a264797b7fb5c349fc
Giorgio Messore <giorgio.messore@live.it>
2025-02-26 18:46:49 +0100

    Fix: Chat updated

--- src/frontEnd/views/EditReqView.vue
+++ src/frontEnd/views/EditReqView.vue
@@ -30,10 +30,10 @@ const jobStore = useJobStore();
 const categories = jobStore.categories;
 let form = reactive({} as any);
 const jobId = router.currentRoute.value.params?.id;
-let job = reactive( jobStore.jobs.find((job) => job._id === jobId) ?? {}) as Job;
+let job = reactive(jobStore.jobs.find((job) => job._id === jobId) ?? {}) as Job;
 
 onBeforeMount(() => {
-  if (router.currentRoute.value.path.includes("edit")) {  
+  if (router.currentRoute.value.path.includes("edit")) {
     if (job) {
       console.log("Dati inviati:", form);
       Object.assign(form, job);
@@ -55,24 +55,27 @@ onBeforeMount(() => {
 
 const handleSubmit = async () => {
   if (router.currentRoute.value.path.includes("edit")) {
-    await jobStore.updateJob(form).then(async() => {
-      await jobStore.updateJobStore(form).then(() => {
+    await jobStore.updateJobStore(form).then(async () => {
+      await jobStore.updateJob(form).then(() => {
         router.replace({ path: "/jobs" });
       });
     });
-    } else {
-      console.log(form);
-      await jobStore.createJob(form).then(async() => {
-        await jobStore.updateJobStore(form).then(() => {
-          router.replace({ path: "/jobs" });
-        });
-      })
+  } else {
+    console.log(form);
+    await jobStore.updateJobStore(form).then(async () => {
+      await jobStore.createJob(form).then(() => {
+        router.replace({ path: "/jobs" });
+      });
+    });
   }
 };
 </script>
 
 <template>
-  <h2 v-if ="router.currentRoute.value.path.includes('edit')" class="text-4xl font-normal text-center p-1 pt-24 mb-4">
+  <h2
+    v-if="router.currentRoute.value.path.includes('edit')"
+    class="text-4xl font-normal text-center p-1 pt-24 mb-4"
+  >
     Modifica richiesta
   </h2>
   <h2 v-else class="text-4xl font-normal text-center p-1 pt-32 md:pt-28 mb-4">
@@ -83,15 +86,17 @@ const handleSubmit = async () => {
     <Card class="mx-2 md:mx-4 lg:mx-auto mb-20 lg:mb-10 max-w-4xl">
       <CardHeader class="text-center">
         <CardDescription class="text-center text-lg font-normal">
-          <h1 v-if="router.currentRoute.value.path.includes('edit')" >Modifica i dati della richiesta</h1>
-          <h1 v-else>Inserisci i dati della richiesta</h1></CardDescription>
+          <h1 v-if="router.currentRoute.value.path.includes('edit')">
+            Modifica i dati della richiesta
+          </h1>
+          <h1 v-else>Inserisci i dati della richiesta</h1></CardDescription
+        >
       </CardHeader>
-      <hr class="mb-6 md:mb-0 md:mt-4 w-3/4 mx-auto">
+      <hr class="mb-6 md:mb-0 md:mt-4 w-3/4 mx-auto" />
       <CardContent>
         <div class="grid gap-4">
           <div class="grid gap-2">
-            <Label for="categories"
-            class="text-sky-950"
+            <Label for="categories" class="text-sky-950"
               >Categoria
               <span v-if="!form.category" class="text-red-500">*</span>
             </Label>
@@ -117,8 +122,7 @@ const handleSubmit = async () => {
             </p>
           </div>
           <div class="grid gap-2">
-            <Label for="title"
-            class="text-sky-950"
+            <Label for="title" class="text-sky-950"
               >Titolo
               <span v-if="!form.title" class="text-red-500">*</span></Label
             >
@@ -134,8 +138,7 @@ const handleSubmit = async () => {
             </p>
           </div>
           <div class="grid gap-2">
-            <Label for="description"
-            class="text-sky-950"
+            <Label for="description" class="text-sky-950"
               >Descrizione
               <span v-if="!form.description" class="text-red-500"
                 >*</span
@@ -152,11 +155,21 @@ const handleSubmit = async () => {
               Questo campo è obbligatorio.
             </p>
           </div>
-          <p class="text-xs italic text-center text-sky-950 opacity-70">Cerca di essere specifico nella tua richiesta così che il Worker possa capire di cosa hai bisogno.</p>
+          <p class="text-xs italic text-center text-sky-950 opacity-70">
+            Cerca di essere specifico nella tua richiesta così che il Worker
+            possa capire di cosa hai bisogno.
+          </p>
         </div>
         <div class="flex justify-center gap-4">
-          <Button @click="router.replace('/jobs')" class="mt-4 primary-btn hover:!text-red-500" type="reset">Annulla</Button>
-          <Button class="mt-4 primary-btn hover:!text-green-500" type="submit">Invia</Button>
+          <Button
+            @click="router.replace('/jobs')"
+            class="mt-4 primary-btn hover:!text-red-500"
+            type="reset"
+            >Annulla</Button
+          >
+          <Button class="mt-4 primary-btn hover:!text-green-500" type="submit"
+            >Invia</Button
+          >
         </div>
       </CardContent>
     </Card>
