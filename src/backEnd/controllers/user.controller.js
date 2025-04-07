6a56178bcbc568955e511fc10e27bd95891b5c98
Giorgio Messore <giorgio.messore@live.it>
2025-04-07 16:33:08 +0200

    Fix: minor updates

--- src/backEnd/controllers/user.controller.js
+++ src/backEnd/controllers/user.controller.js
@@ -136,7 +136,7 @@ const rateWorker = async (req, res) => {
     if (!updatedUser) {
       return res.status(404).json({ message: "Utente non trovato" });
     }
-    res.status(200).end();
+    res.status(200).send();
   } catch (error) {
     console.error("Errore durante l'aggiornamento delle valutazioni:", error);
     res.status(500).json({ message: "Errore del server" });
