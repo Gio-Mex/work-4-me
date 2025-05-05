cdbf6c4c674b6165db557bb8c3d7a6f5ac531a47
Giorgio Messore <giorgio.messore@live.it>
2025-05-05 19:23:48 +0200

    Fix: Middleware added to socket

--- src/frontEnd/stores/userStore.ts
+++ src/frontEnd/stores/userStore.ts
@@ -33,7 +33,6 @@ export const useUserStore = defineStore("user", {
         this.isLoggedIn = true;
         // Connect socket
         socket.connect();
-        socket.emit("connect");
       } catch (error: any) {
         console.error("Errore durante il login:", error);
         // Show message
